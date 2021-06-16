2994a4e06b8682318fc7ff58ac2c076ff430849b
justin_k_m <justinm0623@gmail.com>
2021-06-16 14:46:29 -0400

    small commit, unity assembly issues, need fixing. Removed some recent files

--- Assets/Scripts/Editing/UpdateInteractableBlock.cs
+++ Assets/Scripts/Editing/UpdateInteractableBlock.cs
@@ -6,7 +6,7 @@ public class UpdateInteractableBlock : MonoBehaviour
 {
     [Range(1, 4), Header("Dimensions")]
     public int x = 1;
-    [Range(1, 4)]
+    [Range(1,4)]
     public int y = 1;
 
     [Header("Type")]
@@ -22,73 +22,29 @@ public class UpdateInteractableBlock : MonoBehaviour
     public GameObject question;
     public GameObject hit;
 
-    [Header("Debug")]
-    public bool dontUpdate;
+    private List<GameObject> blocks = new List<GameObject>();
 
-    private List<GameObject> blocks;
-
-    public void OnValidate() 
+    private void OnValidate()
     {
-        UnityEditor.EditorApplication.delayCall += _OnValidate;
+        UpdateColliders();
+        UpdateBlocks();
     }
 
-    // Update when object is changed in editor
-#if UNITY_EDITOR
-    void _OnValidate()
+    void UpdateColliders()
     {
-        if (!dontUpdate)
-        {
-            UpdateObject();
-            UpdateBlocks();
-        }
-    }
-#endif
-
-    void UpdateObject()
-    {
+        Debug.Log("Colliders Running");
         bc.size = new Vector2(x, y);
     }
 
     void UpdateBlocks()
-    {   
+    {
+        Debug.Log(blocks.Count);
         if (blocks.Count < x * y)
         {
             for (int i = blocks.Count; blocks.Count < x * y; i++)
             {
-                Vector2 pos;
-                pos.x = i % x;
-                pos.y = Mathf.Floor(i / y);
-
-                if (custom)
-                {
-                    GameObject obj = new GameObject("Custom " + (i + 1));
-                    obj.transform.parent = transform;
-                    obj.transform.localPosition = pos;
-                }
-
-                if (breakableOnly)
-                {
-                    GameObject obj = Instantiate(breakableTop);
-                    obj.transform.parent = transform;
-                    obj.transform.localPosition = pos;
-                }
+                Debug.Log(i);
             }
         }
-
-        // If size reduced, delete blocks
-        if (blocks.Count > x * y)
-        {
-            for (int i = blocks.Count - 1; blocks.Count > x * y; i--)
-            {
-                DestroyImmediate(transform.GetChild(i).gameObject);
-                blocks.RemoveAt(i);
-            }
-        }
-    }
-
-    public void DestroyChildren()
-    {
-
-        for (int sub = 0; sub < 100; sub++) { for (int i = 0; i < transform.childCount; i++) { DestroyImmediate(transform.GetChild(i).gameObject); } } 
     }
 }
