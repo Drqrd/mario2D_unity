0a3fb0c23693946a6b907db091897cdd9e2b8f16
justin_k_m <justinm0623@gmail.com>
2021-08-16 18:24:49 -0400

    Future work on bounds

---
+++ Assets/Scripts/Controllers/BoundsController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsController : MonoBehaviour
+{
+    public GameObject leftCameraBounds;
+
+    private void Start()
+    {
+        
+    }
+}
